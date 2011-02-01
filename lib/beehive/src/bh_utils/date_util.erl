2ef6a1f4cda794fe04035d9cf20476a824d4ce22
Pete Elmore <pete@debu.gs>
2011-01-31 20:24:25 -0800

    Whitespace cleanup and comments added to the date utils.

--- lib/beehive/src/bh_utils/date_util.erl
+++ lib/beehive/src/bh_utils/date_util.erl
@@ -1,6 +1,7 @@
 -module(date_util).
 -compile(export_all).
 
+% A standard Unix timestamp; use this when talking to the outside world.
 epoch() ->
   now_to_seconds(now()).
 
@@ -9,7 +10,8 @@ time_right_now() ->
   MSec * 1000000 + Sec.
 
 % Calc the elapsed
-calc_elapsed({MSecStart, SecStart, MicroSecStart}, {MSecFinish, SecFinish, MicroSecFinish}) ->
+calc_elapsed({MSecStart, SecStart, MicroSecStart},
+             {MSecFinish, SecFinish, MicroSecFinish}) ->
   (MSecFinish * 1000000 + SecFinish + MicroSecFinish / 1000000) -
   (MSecStart * 1000000 + SecStart + MicroSecStart / 1000000).
 
@@ -19,8 +21,6 @@ epoch_hires() ->
 now_to_seconds({Mega, Sec, _}) -> (Mega * 1000000) + Sec.
 
 seconds_to_timestamp(Seconds) ->
-  % BaseDate      = calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}),
-  % Seconds       = BaseDate + (Milliseconds div 1000),
   calendar:gregorian_seconds_to_datetime(Seconds).
 
 now_to_seconds() -> calendar:datetime_to_gregorian_seconds(calendar:now_to_universal_time(now())).
@@ -50,23 +50,20 @@ gregorian_seconds_to_epoch(Secs) ->
     EpochSecs = epoch_gregorian_seconds(), Secs - EpochSecs.
 
 date_to_epoch(Date) ->
-    datetime_to_epoch({Date, {0,0,0} })
-.
+    datetime_to_epoch({Date, {0,0,0} }).
 
 datetime_to_epoch({Date, Time}) ->
     gregorian_seconds_to_epoch(
-        calendar:datetime_to_gregorian_seconds({Date, Time}))
-.
+        calendar:datetime_to_gregorian_seconds({Date, Time})).
 
 is_older_by(T1, T2, {days, N}) ->
-    N1 = day_difference(T1, T2)
-    , case N1 of
+    N1 = day_difference(T1, T2),
+    case N1 of
         N2 when (-N < N2) ->
             true;
         _ ->
             false
-    end
-.
+    end.
 
 is_sooner_by(T1, T2, {days, N}) ->
     case day_difference(T1, T2) of
@@ -74,13 +71,14 @@ is_sooner_by(T1, T2, {days, N}) ->
             true;
         _ ->
             false
-    end
-.
+    end.
 
 is_time_older_than({Date, Time}, Mark) ->
-    is_time_older_than(calendar:datetime_to_gregorian_seconds({Date, Time}), Mark);
+    is_time_older_than(calendar:datetime_to_gregorian_seconds({Date, Time}),
+      Mark);
 is_time_older_than(Time, {DateMark, TimeMark}) ->
-    is_time_older_than(Time, calendar:datetime_to_gregorian_seconds({DateMark, TimeMark}));
+    is_time_older_than(Time,
+      calendar:datetime_to_gregorian_seconds({DateMark, TimeMark}));
 is_time_older_than(Time, Mark)  when is_integer(Time), is_integer(Mark) ->
     Time < Mark.
 
@@ -89,21 +87,28 @@ day_difference({D1, _}, D2) ->
 day_difference(D1, {D2, _}) ->
     day_difference(D1, D2);
 day_difference(D1, D2) ->
-    Days1 = calendar:date_to_gregorian_days(D1), Days2 = calendar:date_to_gregorian_days(D2), Days1 - Days2.
+    Days1 = calendar:date_to_gregorian_days(D1),
+    Days2 = calendar:date_to_gregorian_days(D2),
+    Days1 - Days2.
 
 is_time_sooner_than({Date, Time}, Mark) ->
-    is_time_sooner_than(calendar:datetime_to_gregorian_seconds({Date, Time}), Mark);
+    is_time_sooner_than(
+      calendar:datetime_to_gregorian_seconds({Date, Time}), Mark);
 is_time_sooner_than(Time, {DateMark, TimeMark}) ->
-    is_time_sooner_than(Time, calendar:datetime_to_gregorian_seconds({DateMark, TimeMark}));
+    is_time_sooner_than(
+      Time, calendar:datetime_to_gregorian_seconds({DateMark, TimeMark}));
 is_time_sooner_than(Time, Mark)  when is_integer(Time), is_integer(Mark) ->
     Time > Mark.
 
 subtract(Date, {days, N}) ->
-    New = calendar:date_to_gregorian_days(Date) - N, calendar:gregorian_days_to_date(New).
+    New = calendar:date_to_gregorian_days(Date) - N,
+    calendar:gregorian_days_to_date(New).
 
 add(Date, {days, N}) ->
-    New = calendar:date_to_gregorian_days(Date) + N, calendar:gregorian_days_to_date(New).
+    New = calendar:date_to_gregorian_days(Date) + N,
+    calendar:gregorian_days_to_date(New).
 
+% Adds a zero to the beginning of a string if there is only one character in it.
 pad_zero(L) when length(L) == 1 -> "0" ++ L;
 pad_zero(L) -> L.
 
