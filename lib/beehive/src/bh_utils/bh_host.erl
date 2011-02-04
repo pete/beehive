9ef48a3f81bc511f70a0185bc9390e8fd37a816c
Pete Elmore <pete@debu.gs>
2011-02-03 17:56:08 -0800

    Tab/whitespace mixture.

--- lib/beehive/src/bh_utils/bh_host.erl
+++ lib/beehive/src/bh_utils/bh_host.erl
@@ -12,9 +12,9 @@
 
 makeip({A1, A2, A3, A4}) ->
   integer_to_list(A1) ++ "." ++
-	integer_to_list(A2) ++ "." ++
-	integer_to_list(A3) ++ "." ++
-	integer_to_list(A4);
+  integer_to_list(A2) ++ "." ++
+  integer_to_list(A3) ++ "." ++
+  integer_to_list(A4);
 
 makeip({A1, A2, A3, A4, A5, A6, A7, A8}) ->
     A = inet_parse:ntoa({A1, A2, A3, A4, A5, A6, A7, A8}),
@@ -25,12 +25,12 @@ ip_string_to_tuple(String) ->
   Tuple.
 
 myip() ->
-    case get_iplist() of
-	[A | _] ->
-	    A;
-	[] ->
-	    "127.0.0.1"
-    end.
+  case get_iplist() of
+    [A | _] ->
+      A;
+    [] ->
+      "127.0.0.1"
+  end.
 
 %% XXX make this return all addresses, currently IPv6 addresses
 %% are not returned!
@@ -44,21 +44,21 @@ get_if([], Res) ->
     Res;
 
 get_if([H | T], Res) ->
-    {ok, B} = inet:ifget(H, [addr, flags]),
-    {value, {flags, Flags}} = lists:keysearch(flags, 1, B),
-    case lists:member(loopback,Flags) of
-	true ->
-	    %% Ignore interfaces with loopback flag
-	    get_if(T, Res);
-	false ->
-	    case lists:keysearch(addr, 1, B) of
-		{value, {addr, Addr}} ->
-		    get_if(T, [makeip(Addr) | Res]);
-		_ ->
-		    %% Interface has no address, might happen on BSD
-		    get_if(T, Res)
-	    end
-    end.
+  {ok, B} = inet:ifget(H, [addr, flags]),
+  {value, {flags, Flags}} = lists:keysearch(flags, 1, B),
+  case lists:member(loopback,Flags) of
+    true ->
+      %% Ignore interfaces with loopback flag
+      get_if(T, Res);
+    false ->
+      case lists:keysearch(addr, 1, B) of
+        {value, {addr, Addr}} ->
+          get_if(T, [makeip(Addr) | Res]);
+        _ ->
+          %% Interface has no address, might happen on BSD
+          get_if(T, Res)
+      end
+  end.
 
 get_iplist() ->
     {ok, If} = inet:getiflist(),
@@ -73,7 +73,8 @@ unused_port_between(Start, End, Acc) ->
   case lists:member(Port, Acc) of
     true -> unused_port_between(Start, End, Acc);
     false ->
-      case gen_tcp:connect("0.0.0.0", Port, [binary, {active, false}, {packet, raw}]) of
+      case gen_tcp:connect("0.0.0.0", Port,
+          [binary, {active, false}, {packet, raw}]) of
         {ok, _S} -> unused_port_between(Start, End, [Port|Acc]);
         _ -> Port
       end
