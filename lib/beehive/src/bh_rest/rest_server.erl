efc7d9a064208a1e7039d761578a238a71d73e83
ed t <edwardt.tril@gmail.com>
2011-02-14 15:11:38 -0800

    fix the unit test

--- lib/beehive/src/bh_rest/rest_server.erl
+++ lib/beehive/src/bh_rest/rest_server.erl
@@ -14,7 +14,7 @@
 -include ("http.hrl").
 
 %% API
--export([start_link/0]).
+-export([start_link/0, stop/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -30,6 +30,9 @@
 %%--------------------------------------------------------------------
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop()->
+  gen_server:cast(?SERVER, stop).
 
 %%====================================================================
 %% gen_server callbacks
