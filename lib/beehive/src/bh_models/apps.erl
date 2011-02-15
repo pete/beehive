0c93e44ae7b7105d2949fdcea2b08810002f5cff
ed t <edwardt.tril@gmail.com>
2011-02-15 12:11:50 -0800

    fix the unit test

--- lib/beehive/src/bh_models/apps.erl
+++ lib/beehive/src/bh_models/apps.erl
@@ -78,9 +78,9 @@ exist(Name) ->
   end.
 
 find_by_name(Name) ->
-  case find_all_by_name(Name) of
-    [H|_Rest] -> H;
+  case find_all_by_name(Name) of\
     [] -> not_found;
+    [H|_Rest] -> H;
     %% This should ALWAYS be not_found, but just to be safe
     E -> E
   end.
